5a309af52808824c216082ccfee23ecceb1143cd
Spyros Zarzonis <spyroszarzonis@gmail.com>
2018-12-07 16:03:36 +0200

    Refactors example project and adds an example for notifications based NetworkDetector

---
+++ Example/NetworkDetector/NotificationsViewController.swift
@@ -0,0 +1,56 @@
+//
+//  NotificationsViewController.swift
+//  NetworkDetector_Example
+//
+//  Created by Spyridon Panagiotis Zarzonis on 07/12/2018.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import UIKit
+import NetworkDetector
+
+class NotificationsViewController: UIViewController {
+    
+    @IBOutlet weak private var networkStatusLabel: UILabel!
+    
+    var networkDetector: NetworkDetector!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        networkDetector = NetworkDetector()
+        NotificationCenter.default.addObserver(self, selector: #selector(networkStatusChanged(_:)), name: .networkStatusChanged, object: networkDetector)
+        
+        do {
+            try networkDetector.startMonitoring()
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+    deinit {
+        networkDetector.stopMonitoring()
+    }
+    
+    @objc private func networkStatusChanged(_ note: Notification) {
+        let networkDetector = note.object as! NetworkDetector
+        
+        switch networkDetector.connection {
+        case .reachable:
+            updateLabelForReachableInternetConnection()
+        case .none:
+            updateLabelForNoInternetConnection()
+        }
+    }
+    
+    private func updateLabelForReachableInternetConnection() {
+        networkStatusLabel.textColor = .green
+        networkStatusLabel.text = "Internet connection is active"
+    }
+    
+    private func updateLabelForNoInternetConnection() {
+        networkStatusLabel.textColor = .red
+        networkStatusLabel.text = "Internet connection is down"
+    }
+}
