53fd35f71e9d55796584e92a470a7c838b2c69bd
Spyros Zarzonis <spyroszarzonis@gmail.com>
2018-12-09 20:04:54 +0200

    Fixes unit tests target

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -1,5 +1,5 @@
 import XCTest
-import NetworkDetector
+@testable import NetworkDetector
 
 class Tests: XCTestCase {
     
@@ -17,12 +17,4 @@ class Tests: XCTestCase {
         // This is an example of a functional test case.
         XCTAssert(true, "Pass")
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure() {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
