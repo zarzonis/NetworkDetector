ca0174ad2b80c674a72917d2c05bcfbe43a368bf
Spyros Zarzonis <spyroszarzonis@gmail.com>
2018-12-09 20:43:11 +0200

    Adds tvOS target

---
+++ Example/NetworkDetector-tvOS Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NetworkDetector-tvOS Example
+//
+//  Created by Spyridon Panagiotis Zarzonis on 09/12/2018.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
