3766a547e22aebd8e5973fcf2b5576362a3fde2f
Spyros Zarzonis <spyroszarzonis@gmail.com>
2018-12-09 18:49:15 +0200

    Adds macOS Example project

---
+++ Example/NetworkDetector-macOS Example/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NetworkDetector-macOS Example
+//
+//  Created by Spyridon Panagiotis Zarzonis on 09/12/2018.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Example/NetworkDetector-macOS Example/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NetworkDetector-macOS Example
+//
+//  Created by Spyridon Panagiotis Zarzonis on 09/12/2018.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
